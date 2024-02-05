0ab4f1375ead75e5a213a4527d4f69618ddbb29d
派派 <2416655925@qq.com>
2024-02-05 17:31:20 +0800

    pixi container

--- app/slider/page.tsx
+++ app/slider/page.tsx
@@ -7,16 +7,19 @@ export default function Slider() {
     useEffect(() => {
         const sketch = new Sketch();
         const canvas = sketch.app.view as HTMLCanvasElement;
-        document.body.appendChild(canvas);
+        const container = document.getElementById('container')!;
+        container.appendChild(canvas);
         canvas.style.display = 'block';
         document.body.style.overscrollBehavior = 'none';
     }, []);
 
     return (<>
-        <h1 className='font-serif text-4xl text-white whitespace-nowrap
+        <div id='container'>
+            <h1 className='font-serif text-4xl text-white whitespace-nowrap
         absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
         mix-blend-difference'>
-            HAVE A NICE DAY!
-        </h1>
+                HAVE A NICE DAY!
+            </h1>
+        </div>
     </>)
 }
